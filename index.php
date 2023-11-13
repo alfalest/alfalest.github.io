dbedad23a1f88b6ab4c124c750339ecc35135fc7
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-11-13 15:26:19 +0700

    update user agent

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" href="assets/logo.png" type="image/png">
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <title>AlfaLest Development</title>
   <style>
     body {
@@ -119,23 +119,72 @@
     .glitch-button:after {
       animation: glitch-anim-2 2s infinite linear alternate-reverse;
     }
+
+    @media only screen and (max-width: 600px) {
+      body {
+        background-color: #0e1111;
+        /* You can adjust the background color for mobile */
+      }
+
+      #desktopContent {
+        display: none;
+        /* Hide desktop content on mobile */
+      }
+
+      #mobileContent {
+        display: block;
+        /* Show mobile content on mobile */
+        text-align: center;
+        padding: 20px;
+      }
+
+      .glitch-text,
+      .glitch-button {
+        font-size: 2em;
+        /* Adjust font size for mobile */
+      }
+
+      .glitch-button {
+        margin-top: 10px;
+        /* Adjust margin for mobile */
+      }
+    }
   </style>
 </head>
+<?php
+// Mendeteksi User-Agent
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+// Menentukan apakah pengguna menggunakan perangkat seluler (misalnya, smartphone atau tablet)
+$isMobile = (strpos($userAgent, 'Mobile') !== false || strpos($userAgent, 'Tablet') !== false);
+?>
 
 <body>
-  <div class="glitch-text" data-text="YOUR SYSTEM WAS DAMAGED BY { ALFALEST }">
-    YOUR SYSTEM WAS DAMAGED BY { ALFALEST }
-  </div>
-  <a href="pages.html" class="glitch-button btn"
-    data-text="CLICK HERE TO REPAIR YOUR SYSTEM">
-    CLICK HERE TO REPAIR YOUR SYSTEM
-  </a>
+  <?php if ($isMobile) : ?>
+    <div id="mobileContent">
+      <div class="glitch-text" data-text="YOUR SYSTEM WAS DAMAGED BY { ALFALEST }">
+        YOUR SYSTEM WAS DAMAGED BY { ALFALEST }
+      </div>
+      <a href="pages.html" class="glitch-button btn" data-text="CLICK HERE TO REPAIR YOUR SYSTEM">
+        CLICK HERE TO REPAIR YOUR SYSTEM
+      </a>
+    </div>
+  <?php else : ?>
+    <div id="desktopContent">
+      <div class="glitch-text" data-text="YOUR SYSTEM WAS DAMAGED BY { ALFALEST }">
+        YOUR SYSTEM WAS DAMAGED BY { ALFALEST }
+      </div>
+      <a href="pages.html" class="glitch-button btn" data-text="CLICK HERE TO REPAIR YOUR SYSTEM">
+        CLICK HERE TO REPAIR YOUR SYSTEM
+      </a>
+    </div>
+
+  <?php endif; ?>
 
   <!-- Scripts -->
   <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
   <script src="assets/js/script.js"></script>
 </body>
+
 </html>
